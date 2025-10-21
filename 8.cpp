7e90fa5af78e958bd4721ec96cf3a4295596690a
Tehmaz Xelilov <tehmazxelilov7@gmail.com>
2025-10-21 17:42:22 +0400

    Delete 8.cpp

--- 8.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a ;
-    cout<<"teref daxil edin : ";
-    cin>>a;
-    cout<<"sahe = "<<a*a<<endl<<"perimetr = "<<4*a<<endl;
-    return 0 ;
-}
